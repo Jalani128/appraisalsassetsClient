701cc1e70678387d03a250e6d93135b4b65d91a1
jilani128 <jilanighulam128@gmail.com>
2026-04-25 10:23:41 +0500

    updata backend url

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:4001/api";
+  process.env.NEXT_PUBLIC_API_URL || "https://appraisalsassets-server.vercel.app/api";
 
 interface ApiResponse<T = any> {
   success: boolean;
